6399e5ee8ef55a33801f77d86151cbe050ce23e0
smibbie <35998407+smibbie@users.noreply.github.com>
2018-11-03 10:43:45 -0400

    refactoring API routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,19 +3,19 @@ import filterParams from "./filterParams";
 
 export default {
   // Gets articles from the NYT API
-  getArticles: function(params) {
+  getArticles: (params) => {
     return axios.get("/api/nyt", { params: filterParams(params) });
   },
   // Gets all saved articles
-  getSavedArticles: function() {
+  getSavedArticles: () => {
     return axios.get("/api/articles");
   },
   // Deletes the saved article with the given id
-  deleteArticle: function(id) {
+  deleteArticle: (id) => {
     return axios.delete("/api/articles/" + id);
   },
   // Saves an article to the database
-  saveArticle: function(articleData) {
+  saveArticle: (articleData) => {
     return axios.post("/api/articles", articleData);
   }
 };
